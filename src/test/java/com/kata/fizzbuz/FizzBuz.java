a64fdc50bcbbca5e4bce2f80228a0e1c85e115ab
Omar <kessemtini.omar@gmail.com>
2023-10-02 13:12:00 +0200

    Fizz case green and it's time to refactor ;)

--- src/test/java/com/kata/fizzbuz/FizzBuz.java
+++ src/test/java/com/kata/fizzbuz/FizzBuz.java
@@ -2,6 +2,15 @@ package com.kata.fizzbuz;
 
 public class FizzBuz {
     public static String execute(int input) {
+        if (input == 3) {
+            return "Fizz";
+        }
+        if (input == 6) {
+            return "Fizz";
+        }
+        if (input == 9) {
+            return "Fizz";
+        }
         return null;
     }
 }
--- src/test/java/com/kata/fizzbuz/FizzBuzTest.java
+++ src/test/java/com/kata/fizzbuz/FizzBuzTest.java
@@ -7,9 +7,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FizzBuzTest {
 
     @Test
-    void shouldReturnFizz() {
+    void shouldReturnFizzForThreeInput() {
         // GIVEN
         int input = 3;
+        String expected = "Fizz";
+
+        // WHEN
+        String result = FizzBuz.execute(input);
+
+        // THEN
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void shouldReturnFizzForSixInput() {
+        // GIVEN
+        int input = 6;
+        String expected = "Fizz";
+
+        // WHEN
+        String result = FizzBuz.execute(input);
+
+        // THEN
+        assertEquals(result, expected);
+    }
+    @Test
+    void shouldReturnFizzForNineInput() {
+        // GIVEN
+        int input = 9;
         String expected = "Fizz";
 
         // WHEN
