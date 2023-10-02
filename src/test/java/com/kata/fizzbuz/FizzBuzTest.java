eda2fdb6ebfb2248cfdcb0ab2156cef1038ee520
Omar <kessemtini.omar@gmail.com>
2023-10-02 12:46:43 +0200

    maven project setup

---
+++ src/test/java/com/kata/fizzbuz/FizzBuzTest.java
@@ -0,0 +1,13 @@
+package com.kata.fizzbuz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FizzBuzTest {
+
+    @Test
+    void justAnExample() {
+        assertEquals(1, 1);
+    }
+}
